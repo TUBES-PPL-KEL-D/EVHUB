3bfb47722cb82157943fad5da0598b9fef207c56
Azkunnn <azkaassidqi32@gmail.com>
2026-04-19 14:47:38 +0700

    [TPD-30] feat: Membuat data koordinat dummy untuk kebutuhan pengerjaan TPD-31.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,8 +4,10 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use App\Models\Vendor;
+use App\Models\Spklu;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -30,6 +32,38 @@ class DatabaseSeeder extends Seeder
             'legality_document_path' => 'dummy/legalitas_pt_energi.pdf',
             'status' => 'Pending',
         ]);
+        // Ambil data vendor pertama (PT Energi Nusantara Raya) yang baru saja dibuat di atasnya
+        $vendor1 = Vendor::first();
+
+        // Memasukkan data dummy SPKLU
+        $spklus = [
+            [
+                'vendor_id' => $vendor1->id, 
+                'name' => 'SPKLU PLN Gedung Sate',
+                'address' => 'Jl. Diponegoro No.22, Citarum, Bandung',
+                'latitude' => -6.902481,
+                'longitude' => 107.618810,
+            ],
+            [
+                'vendor_id' => $vendor1->id,
+                'name' => 'SPKLU PLN Braga CityWalk',
+                'address' => 'Jl. Braga No.99-101, Braga, Bandung',
+                'latitude' => -6.917464,
+                'longitude' => 107.609348,
+            ],
+            [
+                'vendor_id' => $vendor1->id,
+                'name' => 'SPKLU Trans Studio Mall',
+                'address' => 'Jl. Gatot Subroto No.289, Cibangkong, Bandung',
+                'latitude' => -6.925093,
+                'longitude' => 107.636494,
+            ]
+        ];
+
+        foreach ($spklus as $spklu) {
+            Spklu::create($spklu);
+        }
+
 
         // 2. Membuat akun User kedua (sebagai vendor)
         $user2 = User::create([
